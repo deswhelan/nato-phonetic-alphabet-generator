63b661501d8aa42df0ca5ed9f1ff46bdf0c0ddbb
Des Whelan <wheland6@tcd.ie>
2025-11-12 10:48:51 +0000

    handle invalis user input

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ nato_phonetic_alphabet_dict = {item.split(",")[0]:item.split(",")[1] for item in
 
 word_to_code = input("What word would you like to spell using the NATO phonetic alphabet?\n")
 
-coded_word = [nato_phonetic_alphabet_dict[char.upper()] for char in word_to_code]
+input_is_validated = False
 
-print(coded_word)
+while not input_is_validated:
+    try:
+        coded_word = [nato_phonetic_alphabet_dict[char.upper()] for char in word_to_code]
+        input_is_validated = True
+    except KeyError:
+        print("Oops! Only letter are allowed!")
+        word_to_code = input("What word would you like to spell using the NATO phonetic alphabet?\n")
+    else:
+        print(coded_word)
+
+
